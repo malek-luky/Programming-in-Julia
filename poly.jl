cbb799c3432aafa4f91870f1368ba150f5b11ef5
Lukas Malek <malek.luky@gmail.com>
2022-10-07 22:54:11 +0200

    Solution 01-poly

--- poly.jl
+++ poly.jl
@@ -1,14 +1,8 @@
-###
-#
 # BI-JUL: 1. Domácí úkol v B221
 #
 # Podrobný popis zadání naleznete na Course pages nebo ve svém repozitáři.
-#
-
-
-#
-# Typ `Polynomial{T}`
-#
+# 10.7.2022
+# Lukáš Málek
 
 
 """
@@ -24,11 +18,11 @@ Pole musí obsahovat alespoň jeden prvek, jinak nastane chyba.
 Konstruktor správně určí stupeň zadaného polynomu (stupeň nulového polynomu
 bereme jako ``-1``) a případně zahodí nepotřebné nulové koeficienty.
 """
-struct Polynomial{ T <: Number }
+struct Polynomial{T<:Number}
   coefficients::Vector{T}
   degree::Int64
 
-  function Polynomial(coefficients::Vector{T}) where { T <: Number }
+  function Polynomial(coefficients::Vector{T}) where {T<:Number}
     degree = length(coefficients) - 1
 
     if degree == -1
@@ -49,29 +43,24 @@ struct Polynomial{ T <: Number }
 end
 
 
-#
-# Porovnávání polynomů podle zvoleného datového typu.
-#
+"""
+Porovnávání polynomů podle zvoleného datového typu.
+"""
 
 import Base.==
-
-function ==(p::Polynomial{T}, q::Polynomial{T}) where { T <: Number }
+function ==(p::Polynomial{T}, q::Polynomial{T}) where {T<:Number}
   # exact comparison
   p.coefficients == q.coefficients
 end
 
-function ==(p::Polynomial{T}, q::Polynomial{T}) where { T <: AbstractFloat }
+function ==(p::Polynomial{T}, q::Polynomial{T}) where {T<:AbstractFloat}
   # approximate comparison
   p.coefficients ≈ q.coefficients
 end
 
 
-#
-# Pěkná textová reprezentace polynomu.
-#
-
 """
-Ukázka výstupu:
+Textová reprezentace, ukázka výstupu:
 
     julia> Polynomial([1, 2, 3])
     + 3*x^2 + 2*x^1 + 1
@@ -86,7 +75,7 @@ Ukázka výstupu:
     0
 
 """
-function Base.show(io::IO, p::Polynomial{T}) where { T <: Number }
+function Base.show(io::IO, p::Polynomial{T}) where {T<:Number}
   if p.degree <= 0
     return print(io, last(p.coefficients))
   end
@@ -94,7 +83,7 @@ function Base.show(io::IO, p::Polynomial{T}) where { T <: Number }
   tokens = []
 
   for j in p.degree:-1:0
-    c = p.coefficients[j + 1]
+    c = p.coefficients[j+1]
 
     if c < zero(T)
       append!(tokens, "- ", -c)
@@ -111,34 +100,50 @@ function Base.show(io::IO, p::Polynomial{T}) where { T <: Number }
 end
 
 
-#
-# Algebraické operace `+` a `*`
-#
-
-import Base.*, Base.+
-
 """
-Součin dvou polynomů.
+TASK1: Vynásobení dvou plynomů.
 """
-function *(p::Polynomial{T}, q::Polynomial{T}) where { T <: Number }
-  # TODO
 
-  return nothing
-end
-
-"""
-Součet dvou polynomů.
-"""
-function +(p::Polynomial{T}, q::Polynomial{T}) where { T <: Number }
-  # TODO
-
-  return nothing
+import Base.*, Base.+, Base.-
+function *(p::Polynomial{T}, q::Polynomial{T}) where {T<:Number}
+  coefficients = zeros(T, abs(p.degree) + abs(q.degree) + 1)
+  for (index, value) in enumerate(p.coefficients)
+    for (index2, value2) in enumerate(q.coefficients)
+      coefficients[index+index2-1] += value * value2
+    end
+  end
+  return Polynomial(coefficients)
 end
 
 
-#
-# Výpočet funkční hodnoty polynomu
-#
+"""
+TASK 2: Součet dvou polynomů.
+"""
+function +(p::Polynomial{T}, q::Polynomial{T}) where {T<:Number}
+  coefficients = zeros(T, max(p.degree, q.degree) + 1)
+  for (index, value) in enumerate(p.coefficients)
+    coefficients[index] += value
+  end
+  for (index, value) in enumerate(q.coefficients)
+    coefficients[index] += value
+  end
+  return Polynomial(coefficients)
+end
+
+"""
+EXTRA TASK: Odečet dvou polynomů.
+"""
+function -(p::Polynomial{T}, q::Polynomial{T}) where {T<:Number}
+  coefficients = zeros(T, max(p.degree, q.degree) + 1)
+  for (index, value) in enumerate(p.coefficients)
+    coefficients[index] += value
+  end
+  for (index, value) in enumerate(q.coefficients)
+    coefficients[index] -= value
+  end
+  return Polynomial(coefficients)
+end
+
 
 """
 Díky této metodě můžeme polynomy vyhodnocovat, tedy počítat jejich funkční hodnoty.
@@ -151,26 +156,29 @@ Například:
     49//25
 
 """
-function (p::Polynomial{T})(x::S) where { T <: Number, S <: Number }
-  # TODO
-
-  return nothing
+function (p::Polynomial{T})(x::S) where {T<:Number,S<:Number}
+  result = zero(T)
+  for (index, value) in enumerate(p.coefficients)
+    result += value * x^(index - 1)
+  end
+  return result
 end
 
-
-#
-# Dělení polynomu polynomem
-#
 
 """
 Divide polynomial `p` by polynomial `q` and return the quotient and remainder.
 """
-function pdiv(p::Polynomial{T}, q::Polynomial{T}) where { T <: Number }
+function pdiv(p::Polynomial{T}, q::Polynomial{T}) where {T<:Number}
   if q.degree == -1
     throw(ArgumentError("It is not possible to divide by 0!"))
+  else
+    quotient = zeros(T, max(p.degree - q.degree + 1, 1))
+    while p.degree >= q.degree
+      quotient[p.degree-q.degree+1] = p.coefficients[end] / q.coefficients[end]
+      p -= Polynomial([zeros(T, p.degree - q.degree); [convert(T, p.coefficients[end] /  q.coefficients[end])]]) * q
+    end
+    return Polynomial(quotient), p
+
   end
 
-  # TODO
-
-  return nothing
 end
